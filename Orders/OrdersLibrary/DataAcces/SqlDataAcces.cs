2db0cc4218fe742339b36b56868e9916ffeab46f
Radu Nica <radunica20@gmail.com>
2022-02-28 19:36:15 +0200

    Working on the Register Form.

---
+++ Orders/OrdersLibrary/DataAcces/SqlDataAcces.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using OrdersLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersLibrary
+{
+    public class SqlDataAcces
+    {
+       
+        public static List<RoleModel> GetUserTypeClient()
+        {            
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<RoleModel>("SELECT * FROM Role WHERE id='2'", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+        
+        public static void RegisterUsers(UsersModel users)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute(" insert into Register(FirstName,LastName,UserName,Email,Password,RoleId) values(@FirstName,@LastName,@UserName,@Email,@Password,@RoleId)", users);
+
+            }
+        }
+        private static string LoadConnectionString(string id = "Database")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
---
+++ Orders/OrdersUI/RegisterForm.cs
@@ -0,0 +1,71 @@
+﻿using OrdersLibrary;
+using OrdersLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrdersUI
+{
+    public partial class RegisterForm : Form
+    {
+        private List<RoleModel> userType = new List<RoleModel>();
+        public RegisterForm()
+        {
+            InitializeComponent();
+            passwordTextBox.PasswordChar = '*';
+            confirmPasswordTextBox.PasswordChar = '*';
+
+            LoadUserTypeClient();
+        }
+        private void LoadUserTypeClient()
+        {
+            userType = SqlDataAcces.GetUserTypeClient();
+
+            WireUpLists();
+        }
+        
+        private void WireUpLists()
+        {
+            userTypeComboBox.DataSource = null;
+            userTypeComboBox.DataSource = userType;
+            userTypeComboBox.DisplayMember = "Role";
+            
+        }
+
+        private bool ValidateForm()
+        {
+            if(firstNameTextBox.Text.Length == 0)
+            {
+                return false;
+            }
+            if(lastNameTextBox.Text.Length == 0)
+            {
+                return false;
+            }
+
+            if(emailTextBox.Text.Length == 0)
+            {
+                return false;
+            }
+            if(passwordTextBox.Text.Length == 0)
+            {
+                return false;
+            }
+            if(confirmPasswordTextBox.Text.Length == 0)
+            {
+                return false;
+            }            
+            return true;
+        }
+        private void ValidatePassword()
+        {
+            
+        }       
+    }
+}
