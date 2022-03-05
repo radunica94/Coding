7fddbd23da0ca2238fac883f05d9c48f2cbb19d7
Radu Nica <radunica20@gmail.com>
2022-03-05 19:54:53 +0200

    Register Form connected to SQLite

---
+++ OrderSystem/OrderSystemLibrary/SqlDataAcces.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace OrderSystemLibrary
+{
+    public class SqlDataAcces
+    {
+        public static void SaveUser(UsersModel user)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into Register (FirstName, LastName, Email, Password, ConfirmPassword) VALUES(@FirstName, @LastName, @Email, @Password, @ConfirmPassword)", user);
+            }
+        }
+
+        private static string LoadConnectionString(string id = "OrderSystem")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
---
+++ OrderSystem/OrderSystemUI/RegisterForm.cs
@@ -0,0 +1,42 @@
+﻿using OrderSystemLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OrderSystemUI
+{
+    public partial class RegisterForm : Form
+    {
+        List<UsersModel> user = new List<UsersModel>();
+        public RegisterForm()
+        {
+            InitializeComponent();
+            
+        }
+       
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            UsersModel u = new UsersModel();
+
+            u.FirstName = firstNameTextBox.Text;
+            u.LastName = lastNameTextBox.Text;
+            u.Email = emailTextBox.Text;
+            u.Password = passwordTextBox.Text;
+            u.ConfirmPassword = confirmPasswordTextBox.Text;
+
+            SqlDataAcces.SaveUser(u);
+
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            emailTextBox.Text = "";
+            passwordTextBox.Text = "";
+            confirmPasswordTextBox.Text = "";
+        }
+               
+    }
+}
