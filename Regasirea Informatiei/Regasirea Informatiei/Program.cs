60d2701ae11934fa137366f3fe4e720cf29b2cc5
Radu Nica <radunica20@gmail.com>
2022-03-11 12:34:01 +0200

    RegasireInformatiei, creare proiect

---
+++ Regasirea Informatiei/Regasirea Informatiei/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Regasirea_Informatiei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
