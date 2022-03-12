23fe37f3227e29f6c072003af55f8431025e1515
Radu Nica <radunica20@gmail.com>
2022-03-12 17:49:30 +0200

    Creating the game state

---
+++ Tic-Tac-Toe Game/Tic-Tac-Toe Game/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_Game
+{
+    /// <summary>
+    /// enum - clasa care reprezinta un grup de constante
+    /// variabile neschimbate, doar citite.
+    /// </summary>
+    public enum Player
+    {
+        None, X, O
+    }
+}
---
+++ Tic-Tac-Toe Game/Tic-Tac-Toe Game/WinType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_Game
+{
+    public enum WinType
+    {
+        Row, Column, MainDiagonal, AntiDiagonal
+    }
+}
