404712f70ca9c06d9b7d35d0c581c7bf886be597
Radu Nica <radunica20@gmail.com>
2022-03-20 10:08:27 +0200

    Calculator tests.

--- UnitTestExercises/UnitTestExcercises.Tests/CalculatorTests.cs
+++ UnitTestExercises/UnitTestExcercises.Tests/CalculatorTests.cs
@@ -25,5 +25,37 @@ namespace UnitTestExercises.Tests
             Assert.Equal(expected, actual);
             //23:00
         }
+        [Fact]
+        public void Subtract_SimpleValuesShouldCalculate()
+        {
+            Calculator calc = new Calculator();
+            double expected = 10;
+            double actual = calc.Subtract(40, 30);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Multiply_SimpleValuesShouldCalculate()
+        {
+            Calculator calc = new Calculator();
+            double expected = 25;
+            double actual = calc.Multiply(5, 5);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Divide_SimpleValuesShouldCalculate()
+        {
+            Calculator calc = new Calculator();
+            double expected = 9;
+            double actual = calc.Divide(81, 9);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void DivideWithRest_SimpleValuesShoudlCalculate()
+        {
+            Calculator calc = new Calculator();
+            double expected = 7.5;
+            double actual = calc.Divide(15, 2);
+            Assert.Equal(expected, actual);
+        }
     }
 }
