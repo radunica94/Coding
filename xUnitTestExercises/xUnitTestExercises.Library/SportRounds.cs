284e63006e40243b368c95ec2bfa62c91f3b7b42
Radu Nica <radunica20@gmail.com>
2022-03-21 17:56:00 +0200

    SportRounds MEthod + tests

---
+++ xUnitTestExercises/xUnitTestExercises.Library/SportRounds.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTestExercises.Library
+{
+    public class SportRounds
+    {
+        public int CalculateNumberOfRounds(int teams)
+        {
+            int totalEliminationRounds = teams/2;
+            return totalEliminationRounds;
+        }
+    }
+}
---
+++ xUnitTestExercises/xUnitTestExercises.Tests/SportRoundsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using xUnitTestExercises.Library;
+
+namespace xUnitTestExercises.Tests
+{
+    public class SportRoundsTests
+    {
+        [Fact]
+        public void TotalRounds_ShouldPassWithEightTeams()
+        {
+            SportRounds totalRounds = new SportRounds();
+            int expected = 4;
+            int actual = totalRounds.CalculateNumberOfRounds(8);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TotalRounds_ShouldPassWithSixteenTeams()
+        {
+            SportRounds totalRounds = new SportRounds();
+            int expected = 8;
+            int actual = totalRounds.CalculateNumberOfRounds(16);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TotalRounds_ShouldNotPassWithSixteenTeams()
+        {
+            SportRounds totalRounds = new SportRounds();
+            int expected = 9;
+            int actual = totalRounds.CalculateNumberOfRounds(16);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TotalRounds_ShouldNotPassWithSeventeenTeams()
+        {
+            SportRounds totalRounds = new SportRounds();
+            int expected = 7;
+            int actual = totalRounds.CalculateNumberOfRounds(17);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
