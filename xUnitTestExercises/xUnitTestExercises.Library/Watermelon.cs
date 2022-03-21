4f1a629c855f981b27f3ad1feb7822ea9d6406e2
Radu Nica <radunica20@gmail.com>
2022-03-21 14:10:59 +0200

    .

---
+++ xUnitTestExercises/xUnitTestExercises.Library/Watermelon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTestExercises.Library
+{
+    public class Watermelon
+    {
+        public Boolean CheckWatermelonKg(int watermelonWeight)
+        {
+            if(watermelonWeight % 2 == 0)
+            {
+                return true;
+            }   
+            else if(watermelonWeight % 2 == 1)
+            {
+                return false;
+            }
+            return false;   
+        }
+    }
+}
---
+++ xUnitTestExercises/xUnitTestExercises.Tests/WatermelonTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using xUnitTestExercises.Library;
+
+namespace xUnitTestExercises.Tests
+{
+    public class WatermelonTests
+    {
+        [Fact]
+        public void CheckWatermelonWeight_TrueValues()
+        {    
+            Watermelon watermalon = new Watermelon();
+            bool expectation = true;
+            bool actual = watermalon.CheckWatermelonKg(20);
+            Assert.Equal(expectation, actual);  
+        }
+        [Fact]
+        public void CheckWatermelonWeight_FalseValues()
+        {
+            Watermelon watermalon = new Watermelon();
+            bool expectation = false;
+            bool actual = watermalon.CheckWatermelonKg(21);
+            Assert.Equal(expectation, actual);
+        }
+    }
+}
