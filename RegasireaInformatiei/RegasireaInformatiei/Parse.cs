6bea51975fd45b20ed2f8e17ce787969728a0193
Radu Nica <radunica20@gmail.com>
2022-03-26 13:45:28 +0200

    xml folder

---
+++ RegasireaInformatiei/RegasireaInformatiei/Parse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace RegasireaInformatiei
+{
+    public class Parse
+    {
+        private const String filename = @"C:\Coding\RegasireaInformatiei\RegasireaInformatiei\XML\Reuters_34\2504NEWS.XML";
+
+        public static object XmlTextReader { get; private set; }
+
+        static public void Main(String[] args)
+        {
+            
+        }
+    }
+}
